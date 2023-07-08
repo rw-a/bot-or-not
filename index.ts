ff5fe8acab2291a793705e17ad11118e9a74ce10
rw-a <rwa2684@gmail.com>
2023-07-08 16:36:08 +1000

    init express server

---
+++ index.ts
@@ -0,0 +1,20 @@
+import http from 'http';
+import express from 'express';
+import { Server } from "socket.io";
+import { ServerToClientEvents, ClientToServerEvents, InterServerEvents, SocketData } from "./types";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected', socket);
+});
+
+server.listen(3000, () => {
+  console.log('Hosting server on port: 3000');
+});
