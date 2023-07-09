93cbb3228046b16d515f03b7ace01cbd15711e62
rw-a <rwa2684@gmail.com>
2023-07-09 20:32:26 +1000

    removed default vite stuff

--- src/server/main.ts
+++ src/server/main.ts
@@ -4,6 +4,8 @@ import { Server } from "socket.io";
 import ViteExpress from "vite-express";
 import { ServerToClientEvents, ClientToServerEvents, InterServerEvents, SocketData } from "./types";
 
+const WS_PORT = 3000;
+
 const app = express();
 const server = http.createServer(app);
 const io = new Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>(server);
@@ -17,6 +19,6 @@ io.on('connection', (socket) => {
   console.log('A user connected', socket);
 });
 
-ViteExpress.listen(app, 3000, () => {
-  console.log("Server is listening...")
+ViteExpress.listen(app, WS_PORT, () => {
+  console.log(`Server initialised. Listening on port ${WS_PORT}...`)
 });
