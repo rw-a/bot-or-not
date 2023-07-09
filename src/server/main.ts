7401193581836a46f22b00913b23dd2c6d5675e8
rw-a <rwa2684@gmail.com>
2023-07-09 17:58:18 +1000

    migrated to vite-express

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,22 +1,22 @@
 import http from 'http';
 import express from 'express';
 import { Server } from "socket.io";
+import ViteExpress from "vite-express";
 import { ServerToClientEvents, ClientToServerEvents, InterServerEvents, SocketData } from "./types";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>(server);
 
+/*
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
-});
+}); */
 
 io.on('connection', (socket) => {
   console.log('A user connected', socket);
 });
 
-/*
-server.listen(3000, () => {
-  console.log('Hosting server on port: 3000');
+ViteExpress.listen(app, 3000, () => {
+  console.log("Server is listening...")
 });
-*/
