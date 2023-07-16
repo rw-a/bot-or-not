9bb3be020a5d3cea0d35f7951f9cb92a6b0de205
rw-a <rwa2684@gmail.com>
2023-07-16 17:47:55 +1000

    restored config

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,8 @@ export const WS_PORT = 3000;
 
 export const NUMBER_ROUNDS_PER_GAME = 3;
 
-export const WRITING_PHASE_DURATION = 5;   // in seconds
-export const VOTING_PHASE_DURATION = 3;    // in seconds
+export const WRITING_PHASE_DURATION = 60;   // in seconds. will break if time is one hour or more (same with below)
+export const VOTING_PHASE_DURATION = 30;    // in seconds
 export const PHASE_END_LEEWAY_DURATION = 3; // add a few seconds at the end of each phase for requests to arrive
 
 export const POINTS_PER_VOTE = 100;     // for other people voting for your answer
