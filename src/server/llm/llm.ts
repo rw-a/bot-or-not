07b0e6f59e1c08d4b6b4a3a25d832c8e1feace50
rw-a <rwa2684@gmail.com>
2023-07-22 20:11:37 +1000

    added ability to specify exec path

--- src/server/llm/llm.ts
+++ src/server/llm/llm.ts
@@ -4,20 +4,21 @@ import util from 'util';
 const exec = util.promisify(child_process.exec);
 
 export default async function llm(
+        exec_path: string,
         model_path: string, 
         prompt: string, 
         stop: string[] = [], 
         max_tokens: number = 128) {
     
-    const stop_prompts = stop.map((stop_prompt) => `-r "${stop_prompt}"`).join(" ");
-    const command = `./llama-cpp -m "${model_path}" -n ${max_tokens} -p "${prompt}" ${stop_prompts}`;
+    const stop_prompts = (stop) ? stop.map((stop_prompt) => `-r "${stop_prompt}"`).join(" ") : "";
+    const command = `${exec_path} -m "${model_path}" -n ${max_tokens} -p "${prompt}" ${stop_prompts}`;
     
     const { stdout, stderr} = await exec(command);
     return stdout;
 }
 
 function main() {
-    llm("./open_llama-ggml-q4_0.bin", "Q: What is 2+2? A:", ["\n"]);
+    llm("./llama-cpp", "./open_llama-ggml-q4_0.bin", "Q: What is 2+2? A:", ["\n"]);
 }
 
 if (typeof require !== 'undefined' && require.main === module) {
