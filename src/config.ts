58684dc6d20c39200d68ec24742760334a2f4755
rw-a <rwa2684@gmail.com>
2023-07-25 17:58:23 +1000

    reduced phase end leeway

--- src/config.ts
+++ src/config.ts
@@ -10,7 +10,7 @@ export const PHASE_DURATIONS = {
     [GamePhases.Voting]: 30,
     [GamePhases.VotingResults]: 10,
 };
-export const PHASE_END_LEEWAY_DURATION = 3; // add a few seconds at the end of each phase for requests to arrive
+export const PHASE_END_LEEWAY_DURATION = 1; // add a few seconds at the end of each phase for requests to arrive
 
 export const POINTS_PER_VOTE = 500;     // for other people voting for your answer
 export const POINTS_PER_CORRECT_GUESS = 1000;    // for guessing the correct answer
