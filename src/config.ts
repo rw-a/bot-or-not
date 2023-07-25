c37e1dc2310602d5fe06d6302890327fff30a517
rw-a <rwa2684@gmail.com>
2023-07-25 17:57:23 +1000

    adjust points per reward

--- src/config.ts
+++ src/config.ts
@@ -12,8 +12,8 @@ export const PHASE_DURATIONS = {
 };
 export const PHASE_END_LEEWAY_DURATION = 3; // add a few seconds at the end of each phase for requests to arrive
 
-export const POINTS_PER_VOTE = 100;     // for other people voting for your answer
-export const POINTS_PER_CORRECT_GUESS = 200;    // for guessing the correct answer
+export const POINTS_PER_VOTE = 500;     // for other people voting for your answer
+export const POINTS_PER_CORRECT_GUESS = 1000;    // for guessing the correct answer
 
 export const GAME_PHASE_NAMES: {[property in GamePhases]: string} = {
     [GamePhases.Lobby]: "Lobby",
