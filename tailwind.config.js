b376169172c0651cabffd9daf220d65749c812fa
rw-a <rwa2684@gmail.com>
2023-08-31 22:30:21 +1000

    make font less thin

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,17 @@ module.exports = {
     "./src/client/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontWeight: {
+      thin: '200',
+      extralight: '300',
+      light: '400',
+      normal: '500',
+      medium: '600',
+      semibold: '700',
+      bold: '800',
+      extrabold: '900',
+      black: '1000',
+    },
     extend: {
       fontFamily: {
         sans: ['Inconsolata', ...defaultTheme.fontFamily.sans],
